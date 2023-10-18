eb28b04b214e00051a2a6c8924c052108021afb9
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-10-18 21:35:35 +0200

    Update versions / Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,49 @@
 const express = require("express");
 const cors = require("cors");
-const mongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require("mongodb");
 
 const employeesRoutes = require("./routes/employees.routes");
 const departmentsRoutes = require("./routes/departments.routes");
 const productsRoutes = require("./routes/products.routes");
 
-mongoClient.connect(
-  "mongodb://localhost:27017/",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Successfully connected to the database");
-      const db = client.db("companyDB");
+const url = "mongodb://localhost:27017";
+const dbName = "companyDB";
 
-      const app = express();
-      app.use(cors());
-      app.use(express.json());
-      app.use(express.urlencoded({ extended: false }));
+async function startServer() {
+  const client = new MongoClient(url);
 
-      app.use((req, res, next) => {
-        req.db = db;
-        next();
-      });
+  try {
+    await client.connect();
+    console.log("Successfully connected to the database");
+    const db = client.db(dbName);
 
-      app.use("/api", employeesRoutes);
-      app.use("/api", departmentsRoutes);
-      app.use("/api", productsRoutes);
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
-      app.use((req, res) => {
-        res.status(404).send({ message: "Not found..." });
-      });
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
 
-      app.listen("8000", () => {
-        console.log("Server is running on port: 8000");
-      });
-    }
+    app.use("/api", employeesRoutes);
+    app.use("/api", departmentsRoutes);
+    app.use("/api", productsRoutes);
+
+    app.use((req, res) => {
+      res.status(404).send({ message: "Not found..." });
+    });
+
+    app.listen("8000", () => {
+      console.log("Server is running on port: 8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  } finally {
+    // После завершения работы с базой данных закрываем соединение
+    await client.close();
   }
-);
+}
+
+startServer();
