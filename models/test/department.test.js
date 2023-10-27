4a25da98b78c0fd5eb6f5323a0883928c1cdb4b3
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-10-27 09:33:00 +0200

    Add tests for Department

---
+++ models/test/department.test.js
@@ -0,0 +1,20 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+    // Clean models before each test
+    beforeEach(() => {
+        mongoose.models = {};
+    });
+    it('should throw an error if no "name" arg', async () => {
+        const dep = new Department({}); // Create new Department, but don't set `name` attr value
+        dep.validateSync(err => {
+            expect(err.errors.name).to.exist;
+        });
+    });
+    // Once testing is complete, clean the models
+    after(() => {
+        mongoose.models = {};
+    });
+});
